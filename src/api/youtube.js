d91c84ab3f874c52b25a81fd0310a44dfe5132d5
maemeier <maemeier@live.com>
2019-11-22 23:51:08 +0700

    install axios

---
+++ src/api/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyBRKGXRrn7tNpb7iyfs3sZxAiHG-sbTyXs";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResult: 5,
+    key: KEY
+  }
+});
