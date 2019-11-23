541980eef92fe7576f886f9a1d7f980311fdb6f7
maemeier <maemeier@live.com>
2019-11-24 03:39:49 +0700

    fix youtube id

--- src/components/videoDetail.js
+++ src/components/videoDetail.js
@@ -5,11 +5,11 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
 
-  const videoSrc = "https://www.youtube.com/embed/VIDEO_ID";
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
   return (
     <div>
       <div className="ui embed">
-        <iframe title="video player" src={videoSrc} />
+        <iframe title="video player" allow="autoPlay" src={videoSrc} />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
