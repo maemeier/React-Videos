66acb03e25c4cbc65665e48facc81f38b5bf1340
maemeier <maemeier@live.com>
2019-11-24 03:11:03 +0700

    update ui

--- src/components/videoItem.js
+++ src/components/videoItem.js
@@ -5,9 +5,9 @@ const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img
+        alt={video.snippet.title}
         className="ui image"
         src={video.snippet.thumbnails.medium.url}
-        alt="text"
       />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -4,7 +4,13 @@ import VideoItem from "./videoItem";
 const VideoList = ({ videos, onVideoSelect }) => {
   // props.videos
   const renderList = videos.map(video => {
-    return <VideoItem video={video} onVideoSelect={onVideoSelect} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        video={video}
+        onVideoSelect={onVideoSelect}
+      />
+    );
   });
 
   return <div className="ui relaxed divided list"> {renderList}</div>;
